f664986bfb109d621b609f0272efbadcc15e5ca5
Ryan Pethel <arpethel@gmail.com>
2019-07-26 13:26:52 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,9 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
-    # @student = Student.find(params[:id])
   end
 
   def show
-    @post = Post.find(params[:id])
-  end
+  @student = Student.find(params[:id])
+end
 end
