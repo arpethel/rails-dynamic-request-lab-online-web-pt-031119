4c44db8a378cbfa4157f0e3d9b8a0aa3ab84200e
Ryan Pethel <arpethel@gmail.com>
2019-07-26 13:23:44 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,10 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    # @student = Student.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
